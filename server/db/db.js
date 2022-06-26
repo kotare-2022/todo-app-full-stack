95fd0a5a75717bf87982ead6d7ed5b31146a7463
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-26 17:19:20 +1200

    completed GET functionality

--- server/db/db.js
+++ server/db/db.js
@@ -3,31 +3,64 @@ const config = require('./knexfile').development
 
 const connection = knex(config)
 
+// REUSABLES
 function getByTableName(tableName, db = connection) {
   return db(tableName)
     .select()
 }
 
-function getByIdAndTableName(tableName, id, db=connection) {
+function getByIdAndTableName(tableName, id, db = connection) {
   return db(tableName)
     .select()
     .where({id})
 }
 
-function addByTableName(tableName, data, db=connection) {
+function addByTableName(tableName, data, db = connection) {
   return db(tableName)
     .insert(data)
 }
 
-function updateByTableNameAndId(tableName, id, data, db=connection) {
+function updateByTableNameAndId(tableName, id, data, db = connection) {
   return db(tableName)
     .where({id})
     .update(data)
+}
+
+// CUSTOMS
+function getFullTodos(db = connection) {
+  return db('todos')
+    .select(
+      'todos.id AS id',
+      'todos.title AS title',
+      'todos.description AS description',
+      'importance_levels.description AS importance_level_description',
+      'themes.description AS theme_description'
+    )
+    .innerJoin(
+      'importance_levels', 
+      'todos.importance_level_id', 
+      '=',
+      'importance_levels.id'
+    )
+    .innerJoin(
+      'themes',
+      'todos.theme_id',
+      '=',
+      'themes.id'
+    )
+}
+
+function getFullTodoById(id, db = connection) {
+  return getFullTodos()
+    .where({'todos.id' :id}) // need to be more specific
+    .first()
 }
 
 module.exports = {
   getByTableName,
   getByIdAndTableName,
   addByTableName,
-  updateByTableNameAndId
+  updateByTableNameAndId,
+  getFullTodos,
+  getFullTodoById,
 };
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const apiHelper = require('./apiHelper')
+const db = require('../db/db')
 
 const router = express.Router()
 
 router.use(apiHelper.terminalLogger)
 
 // GET /api/v1/todos
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    res.json({ statement: 'Welcome to external APIs!' })
+    const data = await db.getFullTodos()
+    res.status(200).json(data)
   } catch (err) {
-    res.status(500).send(err.message)
+    // consider a logger
+    res.status(500).json({message: err.message})
+    // throw err
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const data = await db.getFullTodoById(id)
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json({message: err.message})
   }
 })
 
