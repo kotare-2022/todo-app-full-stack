7fb7014c813fcfd40f6e1bb08b71537eecf679ad
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-27 03:52:38 +1200

    moved to Todos component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
+import Todos from './Todos'
+
 import { getAllTodos } from '../apiClient'
 
 function App() {
@@ -10,25 +12,15 @@ function App() {
       .then(result => setTodos(result))     
   }, [])
 
+  const AddTodoForm = () => {
+    
+  }
+
+
   return (
     <div className="main">
       <header><h1>Todo Application</h1></header>
-      <div className="todos">
-        {todos && todos.map(todo => {
-          return (
-            <div key={todo.id} className="todo">
-              <div className="todo-header">{todo.title}</div>
-              <div className="todo-description"><em>Description:</em> {todo.description}</div>
-              <div className="todo-importance"><em>Importance:</em> {todo.importance_level_description}</div>
-              <div className="todo-theme"><em>Theme:</em> {todo.theme_description}</div>
-              <div className="todo-buttons">
-                <button className="todo-update" type="submit">Update</button>
-                <button className="todo-delete" type="submit">Delete</button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      <Todos todos={todos}/>
     </div>
   )
 }
---
+++ client/components/Todos.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Todos(props) {
+  const todos = props.todos
+
+  const handleUpdate = () => {
+    console.log('Update not yet implemented')
+  }
+
+  const handleDelete = () => {
+    console.log('Delete not yet implemented')
+  }
+
+  return (
+  <div className="todos">
+    {todos && todos.map(todo => {
+      return (
+        <div key={todo.id} className="todo">
+          <div className="todo-header">{todo.title}</div>
+          <div className="todo-description"><em>Description:</em> {todo.description}</div>
+          <div className="todo-importance"><em>Importance:</em> {todo.importance_level_description}</div>
+          <div className="todo-theme"><em>Theme:</em> {todo.theme_description}</div>
+          <div className="todo-buttons">
+            <button className="todo-update" type="submit" onClick={handleUpdate}>Update</button>
+            <button className="todo-delete" type="submit" onClick={handleDelete}>Delete</button>
+          </div>
+        </div>
+      )
+    })}
+  </div>
+  )
+}
