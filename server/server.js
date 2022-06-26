ba0eb44c8a00c6d35ffb9d0bbc9c4bd92403c38f
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-26 16:01:04 +1200

    added optinal BrowserRouter config on backend

--- server/server.js
+++ server/server.js
@@ -10,4 +10,12 @@ server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/api/v1/todos', todos)
 
+// BrowserRouter config
+/*
+server.get('*', (req, res) => {
+  // note the path is not relative to location of server.js file!
+  res.sendFile(path.resolve('/server/public/index.html'))
+})
+*/
+
 module.exports = server
