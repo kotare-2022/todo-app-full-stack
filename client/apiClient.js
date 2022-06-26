c4fdbb90693da6ab1fb9177b59d6eb7f154fac3a
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-26 15:55:24 +1200

    white space fix

--- client/apiClient.js
+++ client/apiClient.js
@@ -4,31 +4,31 @@ const localApiUrl = '/api/v1/todos'
 
 export async function getAllTodos() {
   const response = await request
-                     .get(localApiUrl)
+    .get(localApiUrl)
   return response.body
 }
 
 export async function getTodoById(id) {
   const response = await request
-                     .get(`${localApiUrl}/${id}`)
+    .get(`${localApiUrl}/${id}`)
   return response.body
 }
 
 export async function addTodo(data) {
   const response = await request
-                     .post(localApiUrl)
-                     .send(data)
+    .post(localApiUrl)
+    .send(data)
   return response.body
 }
 
 export async function updateTodoById(id, data) {
   const reponse = await request
-                     .patch(`${localApiUrl}/${id}`)
-                     .send(data)
+    .patch(`${localApiUrl}/${id}`)
+    .send(data)
   return reponse.body
 }
 
 export async function deleteTodoById(id) {
   await request
-        .delete(`${localApiUrl}/${id}`)
+    .delete(`${localApiUrl}/${id}`)
 }
