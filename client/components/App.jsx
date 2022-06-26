185abc95d4e3c3b4dd36a253c76df42f297ea8ce
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-26 18:11:38 +1200

    added front end skeleton todo display

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from 'react'
 
+import { getAllTodos } from '../apiClient'
+
 function App() {
-  
+  const [todos, setTodos] = useState(null)
+
+  useEffect(() => {
+    getAllTodos()
+      .then(result => setTodos(result))     
+  }, [])
 
   return (
-    <h1>Hello World!</h1>
+    <div className="main">
+      <h1>Todo Application</h1>
+      <ul>
+        {todos && todos.map(todo => {
+          
+          return (
+            <li key={todo.id}>
+                <p>Title: {todo.title}</p>
+                <p>Description: {todo.description}</p>
+                <p>Importance: {todo.importance_level_description}</p>
+                <p>Theme: {todo.theme_description}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
