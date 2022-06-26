74c0fad4c6109152942e80d7e7bfe739d74e25d8
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-27 08:11:23 +1200

    basic form setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react'
 import Todos from './Todos'
 
 import todosServices from '../services/todos'
+import themeServices from '../services/themes'
+import importanceServices from '../services/importance'
 
 function App() {
   const [todos, setTodos] = useState(null)
@@ -15,10 +17,21 @@ function App() {
   const AddTodoForm = () => {
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
-    const [theme, setTheme] = useState([])
+    const [theme, setTheme] = useState('')
+    const [themes, setThemes] = useState([])
     const [importance, setImportance] = useState('')
+    const [importanceLevels, setImportanceLevels] = useState([])
 
     useEffect(() => {
+      const themeRequest = themeServices
+        .getAllThemes()
+      const importanceRequest = importanceServices
+        .getAllImportanceLevels()
+      Promise.allSettled([themeRequest, importanceRequest])
+        .then(result => {
+          setThemes(result[0].value) // first & second at collection
+          setImportanceLevels(result[1].value)
+        })
     }, [])
 
     return (
@@ -34,8 +47,28 @@ function App() {
           value={description} onChange={e => setDescription(e.target.value)}
         />
         <label htmlFor="theme">Choose a Theme:</label>
-        <select name="theme" id="theme">
-          
+        <select 
+          name="theme" id="theme" 
+          value={theme} 
+          onChange={e => setTheme(e.target.value)}
+        >
+          {themes.map(t => {
+            // t.id and t.description
+            const [key, descr] = [t.id, t.description]
+            return <option key={key} value={descr}>{descr}</option>
+          })}
+        </select>
+        <label htmlFor="importance_level">How important is this:</label>
+        <select 
+          name="importance_level" id="importance_level" 
+          value={importance} 
+          onChange={e => setImportance(e.target.value)}
+        >
+          {importanceLevels.map(imp => {
+            // imp.id and imp.description
+            const [key, descr] = [imp.id, imp.description]
+            return <option key={key} value={descr}>{descr}</option>
+          })}
         </select>
       </form>
     )
